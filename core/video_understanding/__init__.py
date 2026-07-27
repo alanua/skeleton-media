32890273f77639c6a6a06b061ac7c3f05acc6096
alanua <alanua@users.noreply.github.com>
2026-07-28 01:33:31 +0200

    Export Phase B video runtime components

--- core/video_understanding/__init__.py
+++ core/video_understanding/__init__.py
@@ -1,4 +1,13 @@
+from core.video_understanding.artifact_store import FinalizedArtifact, PrivateArtifactStore
+from core.video_understanding.compatibility import DIOS_OPERATION_MAP, map_dios_operation
 from core.video_understanding.domain_router import DomainRoute, route_domain
+from core.video_understanding.intake_adapters import (
+    AcquiredSource,
+    DirectMediaAdapter,
+    LocalFileAdapter,
+    SourceMetadata,
+    YtDlpAdapter,
+)
 from core.video_understanding.local_llm import LocalLlmClient, LocalLlmConfig, local_llm_policy
 from core.video_understanding.manifest import ArtifactEntry, ArtifactManifest, build_manifest
 from core.video_understanding.memory_gateway_adapter import build_private_mutation
@@ -24,42 +33,73 @@ from core.video_understanding.models import (
     validate_transition,
 )
 from core.video_understanding.operations import OPERATIONS, plan_operation
+from core.video_understanding.pipeline import PipelineResult, PrivateBridgeOllama, VideoPipeline
+from core.video_understanding.queue import FileQueue, QueueRecord
+from core.video_understanding.runtime_config import RuntimeLimits, VideoRuntimeConfig, load_runtime_config
+from core.video_understanding.sona_backend import SonaBackend, SonaProcessManager
+from core.video_understanding.transcript import TranscriptQuality, TranscriptSegment
 from core.video_understanding.url_classifier import (
     SourceClassification,
     classify_local_reference,
     classify_remote_url,
 )
+from core.video_understanding.vision import FrameArtifact, MediaProbe
+from core.video_understanding.worker import VideoWorker
 
 __all__ = [
+    "AcquiredSource",
     "ArtifactEntry",
     "ArtifactManifest",
     "Claim",
+    "DIOS_OPERATION_MAP",
+    "DirectMediaAdapter",
     "Domain",
     "DomainRoute",
     "Entity",
+    "FileQueue",
+    "FinalizedArtifact",
+    "FrameArtifact",
     "FrameEvidence",
+    "LocalFileAdapter",
     "LocalLlmClient",
     "LocalLlmConfig",
+    "MediaProbe",
     "OPERATIONS",
+    "PipelineResult",
+    "PrivateArtifactStore",
+    "PrivateBridgeOllama",
     "ProcessingMode",
     "ProcessingState",
     "ProjectLink",
+    "QueueRecord",
     "ReviewDecision",
+    "RuntimeLimits",
+    "SonaBackend",
+    "SonaProcessManager",
     "SourceClassification",
+    "SourceMetadata",
     "SourceReference",
     "SupportType",
     "TimestampedEvidence",
     "Topic",
     "TranscriptArtifact",
+    "TranscriptQuality",
+    "TranscriptSegment",
+    "VideoPipeline",
     "VideoRecord",
     "VideoRequest",
+    "VideoRuntimeConfig",
     "VideoUnderstandingError",
+    "VideoWorker",
     "Workflow",
+    "YtDlpAdapter",
     "build_manifest",
     "build_private_mutation",
     "classify_local_reference",
     "classify_remote_url",
+    "load_runtime_config",
     "local_llm_policy",
+    "map_dios_operation",
     "plan_operation",
     "public_receipt",
     "route_domain",
